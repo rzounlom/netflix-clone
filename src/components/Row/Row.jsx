e053263ce29902553479b21794dfd3b2324cac96
rzounlome <rzounlome@harmonyhit.com>
2020-12-04 16:22:36 -0500

    added movie trailer and reac-youtube packages to dinamically play movie trailers

--- src/components/Row/Row.jsx
+++ src/components/Row/Row.jsx
@@ -2,9 +2,13 @@ import React, { useState, useEffect } from "react";
 import axios from "../../axios/axios";
 import "./Row.scss";
 
+import YouTube from "react-youtube";
+import movieTrailer from "movie-trailer";
+
 const Row = ({ title, fetchUrl, isLargeRow }) => {
   const baseNetflixOriginalMovieImgUrl = "https://image.tmdb.org/t/p/original";
   const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
 
   useEffect(() => {
     const fetchData = async () => {
@@ -15,21 +19,49 @@ const Row = ({ title, fetchUrl, isLargeRow }) => {
 
     fetchData();
   }, [fetchUrl]);
-  const listMovies = movies.map((movie) => (
-    <img
-      className={`row__poster ${isLargeRow && "row__posterLarge"}`}
-      key={movie.id}
-      src={`${baseNetflixOriginalMovieImgUrl}${
-        isLargeRow ? movie.poster_path : movie.backdrop_path
-      }`}
-      alt={movie.name}
-    ></img>
-  ));
+
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 1,
+    },
+  };
+
+  const handleMovieUrl = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      movieTrailer(movie?.name || "")
+        .then((url) => {
+          //https://www.youtube.com/watch?v=XtMThy8QKqU
+          const urlParams = new URLSearchParams(new URL(url).search); //get url params (everything after question mark)
+          setTrailerUrl(urlParams.get("v"));
+        })
+        .catch((error) => console.log(error));
+    }
+  };
+
+  console.log("trailer url: " + trailerUrl);
 
   return (
     <div className="row">
       <h2>{title}</h2>
-      <div className="row__posters">{listMovies}</div>
+      <div className="row__posters">
+        {movies.map((movie) => (
+          <img
+            className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+            key={movie.id}
+            src={`${baseNetflixOriginalMovieImgUrl}${
+              isLargeRow ? movie.poster_path : movie.backdrop_path
+            }`}
+            alt={movie.name}
+            onClick={handleMovieUrl(movie)}
+          ></img>
+        ))}
+      </div>
+      {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
     </div>
   );
 };
