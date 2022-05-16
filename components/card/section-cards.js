0ce8c215514817aaf081e2e71b17bbdba4bb6b50
Romaric Zounlome <rzounlom@gmail.com>
2022-05-16 11:11:39 -0400

    add video json file

--- components/card/section-cards.js
+++ components/card/section-cards.js
@@ -1,6 +1,7 @@
 import Card from "../card";
 import Link from "next/link";
 import clsx from "classnames";
+import { getVideos } from "../../lib/videos";
 import styles from "./section-cards.module.css";
 
 const SectionCards = ({
@@ -10,12 +11,13 @@ const SectionCards = ({
   shouldWrap = false,
   shouldScale,
 }) => {
-  console.log({ videos });
+  const disneyVideos = getVideos();
+  console.log({ disneyVideos });
   return (
     <section className={styles.container}>
       <h2 className={styles.title}>{title}</h2>
       <div className={clsx(styles.cardWrapper, shouldWrap && styles.wrap)}>
-        {videos.map((video, idx) => {
+        {disneyVideos.map((video, idx) => {
           return (
             <Link key={idx} href={`/video/${video.id}`}>
               <a>
