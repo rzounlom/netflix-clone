638c30e3e2dcaf2187eb4519619cad510975672b
Romaric Zounlome <rzounlom@gmail.com>
2022-05-20 04:33:56 -0400

    add loader

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,44 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+
+import { useEffect, useState } from "react";
+
+import Loading from "../components/loading";
+import { magic } from "../lib/magic-client";
+import { useRouter } from "next/router";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    const confirmLogin = async () => {
+      try {
+        const isLoggedIn = await magic.user.isLoggedIn();
+        if (isLoggedIn) {
+          router.push("/");
+        } else router.push("/login");
+      } catch (error) {
+        console.log("Error verifying user loggin", error);
+        router.push("/login");
+      }
+    };
+
+    confirmLogin();
+  }, []);
+
+  useEffect(() => {
+    const handleComplete = () => {
+      setIsLoading(false);
+    };
+    router.events.on("routeChangeComplete", handleComplete);
+    router.events.on("routeChangeError", handleComplete);
+
+    return () => {
+      router.events.off("routeChangeComplete", handleComplete);
+      router.events.off("routeChangeError", handleComplete);
+    };
+  }, [router]);
+
+  return isLoading ? <Loading /> : <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
